6771e151b71aa1a2137e51368ecb50c5a242da6a
Alex <spawnaga@gmail.com>
2024-05-07 04:34:20 -0700

    autofix and run prettier

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'Asclepius Systems',
-  title: 'Asclepius Starter',
-  description: 'Sign up to start building your medical business',
-  locale: 'en',
+  site_name: "Asclepius Systems",
+  title: "Asclepius Starter",
+  description: "Sign up to start building your medical business",
+  locale: "en",
 };
